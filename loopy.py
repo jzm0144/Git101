711b6e73f35a0e21dae5e20e9fd8bd46254b19d4
jzm0144 <jzm0144@auburn.edu>
2023-12-08 18:11:06 +0500

    branch_b: added the feature to print in desc order

--- loopy.py
+++ loopy.py
@@ -6,4 +6,7 @@ print("printing numbers from 1 to 10 using a *for* loop")
 for i in range(10):
     print(i+1)
 
-print("\n\n printing the same 10 numbers upside down")
+print("\n\nprinting the same 10 numbers upside down")
+for i in range(10):
+    print(10-i)
+
